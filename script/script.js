89d892f9f6a3e4efae0a6539b8a5c8d49d8e679a
Aldi Workpace <aldirizkibagus905@gmail.com>
2025-12-29 11:24:05 +0700

    upd

---
+++ script/script.js
@@ -0,0 +1,12 @@
+
+//nav menu tombol//
+
+function bukamenu() {
+    const menusam = document.querySelector('.menusamping')
+    menusam.style.display = 'flex'
+}
+
+function sembunyimenu() {
+    const menusam = document.querySelector('.menusamping')
+    menusam.style.display = 'none'
+}
